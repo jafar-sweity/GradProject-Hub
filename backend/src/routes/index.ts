33af7a3a9e0198b88b920e438e9f44546b10b067
Ahmad <nazzala8@gmail.com>
2024-11-13 16:16:48 +0200

    Add Backend for Email service

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import userRoutes from "./user.js";
 import taskRoutes from "./task.js";
 import authRoutes from "./auth.js";
 import projectRoutes from "./project.js";
+import emailRoutes from "./email.js";
 // import subTaskRoutes from "./subTask.js";
 const routes = Router();
 
@@ -14,6 +15,7 @@ routes.use("/api/v1/auth", authRoutes);
 routes.use("/api/v1/users", userRoutes);
 routes.use("/api/v1/projects", projectRoutes);
 routes.use("/api/v1/projects/:projectId/tasks", taskRoutes);
+routes.use("/api/v1/emails", emailRoutes);
 // routes.use("/api/v1/projects/:projectId/tasks/:taskId/subTasks", subTaskRoutes);
 
 export default routes;
